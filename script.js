3e8f837ac36ebc58eff7b1aedf2331ce717ba026
Patrick Snijder <76705330+psnijder@users.noreply.github.com>
2024-11-25 22:06:18 +0100

    Set up hover effect on squares

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const square = document.createElement("div");
 square.className = "square";
 
 for (let i = 0; i < 256; i++) {
-    //square.innerText = i;
-    document.getElementById("grid-container").appendChild(square.cloneNode(true)); 
+    document.getElementById("grid-container").appendChild(square.cloneNode(true));
 }
+
+const clonedSquares = document.querySelectorAll(".square");
+
+clonedSquares.forEach(square => {
+    square.addEventListener("mouseenter", () => {
+        square.style.backgroundColor = "black";
+    });
+});
